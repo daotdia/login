216a175174d4afde2d6fd52c6ef625fa0d885469
David Otero Diaz <daotdia@gmail.com>
2023-06-16 02:20:29 +0200

    Inicio del proyecto basico de spring para autenticar a un usuario

---
+++ src/main/java/com/example/davidotero/general_login/GeneralLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.davidotero.general_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/davidotero/general_login/GeneralLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.davidotero.general_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
